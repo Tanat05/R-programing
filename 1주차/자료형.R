59a0beb31d18cae65407df5fca12197aaaa59088
Tanat <85154556+Tanat05@users.noreply.github.com>
2024-09-03 10:31:08 +0900

    Update 자료형.R

--- 1주차/자료형.R
+++ 1주차/자료형.R
@@ -1,6 +1,6 @@
-'first' #"first"
-"Second" #"Second"
+'first' #[1] "first"
+"Second" #[1] "Second"
 First #error
 
-class('1') #"character"
-class(1) #"numeric"
+class('1') #[1] "character"
+class(1) #[1] "numeric"
