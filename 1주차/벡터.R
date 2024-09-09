ab46e9a94d5241c672d87f3cb929c14e48e63746
Tanat <85154556+Tanat05@users.noreply.github.com>
2024-09-09 14:16:12 +0900

    Update 벡터.R

--- 1주차/벡터.R
+++ 1주차/벡터.R
@@ -36,6 +36,8 @@ append(vec1,10,after=3)
 append(vec1,c(10,11),after=3)
 #[1]  1  6  3 10 11  4  5  7 NA NA  9
 
+
+#사칙연산
 c(1,2,3) + c(4,5,6)
 #[1] 5 7 9
 c(1,2,3) + 1
@@ -48,6 +50,8 @@ var3 <- c('3','4',5)
 var1 + var3
 #error
 
+
+#순환연산
 var4 <- c(1,2,3,4,5)
 var1
 #[1] 1 2 3
@@ -57,6 +61,8 @@ var1 + var4
 #[1] 2 4 6 7
 #error
 
+
+#백터의 길이
 var1
 #[1] 1 2 3
 length(var1)
@@ -66,3 +72,56 @@ NROW(var1)
 
 #length() 백터의 요소 갯수
 #length() = NROW()
+
+
+#벡터에 문자 포함 여부
+var7 <- seq(from=1, to=9, by=2)
+var7
+#[1] 1 3 5 7 9
+3 %in% var7
+#[1] TRUE
+4 %in% var7
+#[1] FALSE
+
+
+#벡터 조작(manipulation)
+z <- c(5,2,-3,8)
+w <- z[z *z >8]
+w
+#[1]  5 -3  8
+z <- c(5,2,-3,8)
+z
+#[1]  5  2 -3  8
+z *z>8
+#[1]  TRUE FALSE  TRUE  TRUE
+
+x <- c(6,1:3,NA,12)
+x
+#[1]  6  1  2  3 NA 12
+x[x > 5]
+#[1]  6 NA 12
+subset(x, x > 5)
+#[1]  6 12
+
+z <- c(5,2,-3,8)
+which(z*z > 8)
+#[1] 1 3 4
+
+fruits <- c(10,20,30)
+fruits
+names(fruits) <- c('apple', 'banana', 'peach')
+fruits
+# apple banana  peach 
+#    10     20     30
+names(fruits)
+#[1] "apple"  "banana" "peach"
+
+
+#문제
+#1. 다음과 같이 vec1을 생성하시오
+vec1 <- c('사과', '배', '감', '버섯', '고구마')
+vec1
+#[1] "사과"   "배"     "감"     "버섯"   "고구마"
+#2. Vec1에서 3번째 요소인 ‘감’을 제외하고 vec1의 값을 출력하시오.
+vec1[-3]
+#[1] "사과"   "배"     "버섯"   "고구마"
