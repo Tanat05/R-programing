d634927cfbb8719230910c99b9b150f3fb190d4f
Tanat <85154556+Tanat05@users.noreply.github.com>
2024-09-23 14:23:17 +0900

    Update 데이터 프레임.r

--- 4주차/데이터 프레임.r
+++ 4주차/데이터 프레임.r
@@ -1,3 +1,4 @@
+#데이터프레임 생성하
 no <- c(1,2,3,4)
 name <- c('Apple','Peach','Banna','Grape')
 price <- c(500,200,100,50)
@@ -97,3 +98,109 @@ sales[,c(1:3)]
 #4  4 Grape    50
 
 #조건추출
+sales
+#  NO  NAME PRICE QTY
+#1  1 Apple   500   5
+#2  2 Peach   200   2
+#3  3 Banna   100   4
+#4  4 Grape    50   7
+subset(sales,QTY<5)
+#  NO  NAME PRICE QTY
+#2  2 Peach   200   2
+#3  3 Banna   100   4
+subset(sales,PRICE==200)
+#  NO  NAME PRICE QTY
+#2  2 Peach   200   2
+
+subset(sales, NAME==Apple)
+#error
+subset(sales, NAME=='Apple')
+#  NO  NAME PRICE QTY
+#1  1 Apple   500   5
+
+#연습문제
+df1 <- data.frame(NAME=c('apple','banna','cherry'),QTY=c(300,200,100))
+df1
+#    NAME QTY
+#1  apple 300
+#2  banna 200
+#3 cherry 100
+df2 <- data.frame(NAME=c('apple','cherry','berry'),PRICE=c(1000,2000,3000))
+df2
+#    NAME PRICE
+#1  apple  1000
+#2 cherry  2000
+#3  berry  3000
+
+df3 <- cbind(df1,df2)
+df3
+#    NAME QTY   NAME  QTY
+#1  apple 300  apple 1000
+#2  banna 200 cherry 2000
+#3 cherry 100  berry 3000
+
+df4 <- rbind(df1,df1)
+df4
+#    NAME QTY
+#1  apple 300
+#2  banna 200
+#3 cherry 100
+#  apple 300
+#5  banna 200
+#6 cherry 100
+
+#데이터프레임 합치기
+df1
+    NAME QTY
+#1  apple 300
+#2  banna 200
+#3 cherry 100
+df2
+#    NAME PRICE
+#1  apple  1000
+#2 cherry  2000
+#3  berry  3000
+merge(df1,df2)
+#    NAME QTY PRICE
+#1  apple 300  1000
+#2 cherry 100  2000
+
+merge(df1,df2,all=T)
+#    NAME QTY PRICE
+#1  apple 300  1000
+#2  banna 200    NA
+#3  berry  NA  3000
+#4 cherry 100  2000
+
+df1
+#    NAME QTY
+#1  apple 300
+#2  banna 200
+#3 cherry 100
+new <- data.frame(NAME="mango",QTY=400)
+df1 <- rbind(df1,new)
+df1
+#    NAME QTY
+#1  apple 300
+#2  banna 200
+#3 cherry 100
+#4  mango 400
+
+#연습문제
+df1 <- data.frame(NAME=c('apple','banana','cherry','mango','berry'), PRICE=c(300,200,100,400,500))
+df1
+#    NAME PRICE
+#1  apple   300
+#2 banana   200
+#3 cherry   100
+#4  mango   400
+#5  berry   500
+new <- data.frame(NAME=c('apple','banana','cherry','mango','berry'), QTY=c(10,20,30,40,50))
+df1 <- merge(df1,new)
+df1
+#    NAME PRICE QTY
+#1  apple   300  10
+#2 banana   200  20
+#3  berry   500  50
+#4 cherry   100  30
+#5  mango   400  40
