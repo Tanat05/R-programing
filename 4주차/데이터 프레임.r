fce3735138200553ac5907a8edfe99958df4eb9d
Tanat <85154556+Tanat05@users.noreply.github.com>
2024-09-23 13:56:53 +0900

    Update 데이터 프레임.r

--- 4주차/데이터 프레임.r
+++ 4주차/데이터 프레임.r
@@ -9,3 +9,68 @@ sales
 #2  2 Peach   200   2
 #3  3 Banna   100   4
 #4  4 Grape    50   7
+
+sales2 <- matrix(c(1,'Apple',500,5,2,'Peach',200,2,3,'Banna',100,4,4,'Grape',50,7), nrow=4, byrow=T)
+sales2
+#     [,1] [,2]    [,3]  [,4]
+#[1,] "1"  "Apple" "500" "5" 
+#[2,] "2"  "Peach" "200" "2" 
+#[3,] "3"  "Banna" "100" "4" 
+#[4,] "4"  "Grape" "50"  "7"
+
+df1 <- data.frame(sales2)
+df1
+#  X1    X2  X3 X4
+#1  1 Apple 500  5
+#2  2 Peach 200  2
+#3  3 Banna 100  4
+#4  4 Grape  50  7
+
+class(df1)
+#[1] "data.frame"
+class(vector)
+#[1] "function"
+
+sapply(df1, class)
+#         X1          X2          X3          X4 
+#"character" "character" "character" "character"
+
+df2 <- transform(df1, X1=as.numeric(X1))
+df2
+#  X1    X2  X3 X4
+#1  1 Apple 500  5
+#2  2 Peach 200  2
+#3  3 Banna 100  4
+#4  4 Grape  50  7
+
+list1 <- list(name='James Seo', address='Seoul', tel='010-8706-4712', pay=500)
+list2 <- list(name='James Seo', address='Seoul', tel='010-8076-4712', pay=500)
+a <- rbind(data.frame(list1), list2)
+a
+#       name address           tel pay
+#1 James Seo   Seoul 010-8706-4712 500
+#2 James Seo   Seoul 010-8076-4712 500
+
+names(df1) <- c('NO', 'NMAE','PRICE','QTY')
+df1
+#  NO  NMAE PRICE QTY
+#1  1 Apple   500   5
+#2  2 Peach   200   2
+#3  3 Banna   100   4
+#4  4 Grape    50   7
+
+sales
+#  NO  NAME PRICE QTY
+#1  1 Apple   500   5
+#2  2 Peach   200   2
+#3  3 Banna   100   4
+#4  4 Grape    50   7
+sales$NAME
+#[1] "Apple" "Peach" "Banna" "Grape"
+sales[1,3]
+#[1] 500
+sales[1,]
+#  NO  NAME PRICE QTY
+#1  1 Apple   500   5
+sales[,3]
+#[1] 500 200 100  50
