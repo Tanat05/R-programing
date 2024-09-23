0d7cf82732fbcd0ddd06fc9fee8f7a7fe4ec709b
Tanat <85154556+Tanat05@users.noreply.github.com>
2024-09-23 13:40:52 +0900

    Create 데이터 프레임.r

---
+++ 4주차/데이터 프레임.r
@@ -0,0 +1,11 @@
+no <- c(1,2,3,4)
+name <- c('Apple','Peach','Banna','Grape')
+price <- c(500,200,100,50)
+qty <- c(5,2,4,7)
+sales <- data.frame(NO=no, NAME=name,PRICE=price, QTY=qty)
+sales
+#  NO  NAME PRICE QTY
+#1  1 Apple   500   5
+#2  2 Peach   200   2
+#3  3 Banna   100   4
+#4  4 Grape    50   7
