9241d4bd8bcc8d3f59b7278d10af2e3088871ba8
Tanat <85154556+Tanat05@users.noreply.github.com>
2024-09-24 10:16:27 +0900

    Update 데이터 프레임.r

--- 4주차/데이터 프레임.r
+++ 4주차/데이터 프레임.r
@@ -198,6 +198,9 @@ df1
 new <- data.frame(NAME=c('apple','banana','cherry','mango','berry'), QTY=c(10,20,30,40,50))
 df1 <- merge(df1,new)
 df1
+#또는
+df1 <- cbind(df1, c(10,20,30,40,50))
+df1
 #    NAME PRICE QTY
 #1  apple   300  10
 #2 banana   200  20
