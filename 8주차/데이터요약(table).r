51ab31f72937107a2fc0684ac0240ae036e2ffd4
Tanat <85154556+Tanat05@users.noreply.github.com>
2024-10-29 11:27:42 +0900

    Create 데이터요약(table).r

---
+++ 8주차/데이터요약(table).r
@@ -0,0 +1,17 @@
+library(googleVis)
+#table로 요약
+table_result <- table(Fruits["Fruit"])
+table_result
+
+table_result <- table(Fruits[c("Fruit", "Location")])
+table_result
+class(table_result)
+
+table_result[1,2]
+table_result["Oranges", "East"]
+
+#table > dataframe
+d_table <- as.data.frame(table_result)
+d_table
+
+subset(d_table, Freq>0)
