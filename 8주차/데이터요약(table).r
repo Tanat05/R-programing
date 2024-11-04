6350f3acbf0573a502e0b91827c2ca77326893f7
Tanat <85154556+Tanat05@users.noreply.github.com>
2024-11-04 13:42:27 +0900

    Update 데이터요약(table).r

--- 8주차/데이터요약(table).r
+++ 8주차/데이터요약(table).r
@@ -15,4 +15,24 @@ d_table <- as.data.frame(table_result)
 d_table
 
 #연습문제
+install.packages("dplyr")
+library(dplyr)
+filter(d_table, Freq>0)
 subset(d_table, Freq>0)
+
+#종합예제
+install.packages("MASS")
+library(MASS)
+str(Cars93)
+#Type, Origin, MPG.city, MPG.highway 으로만 데이터프레임 생성
+Cars93_dataframe <- Cars93[c("Type", "Origin", "MPG.city", "MPG.highway")]
+#차종 별 데이터 개수 산출
+summary(Cars93_dataframe["Type"])
+#이중에서 Compact와 Van만 선별
+subset(Cars93_dataframe, Type=="Compact"|Type=="Van")
+
+library(reshape2)
+Cars93_sample_melt <-  melt(Cars93_dataframe, id=c("Type", "Origin"), variable.names="variable", value.name="value")
+Cars93_sample_melt
+
+dcast(data=Cars93_sample_melt, Type~variable, fun=mean)
