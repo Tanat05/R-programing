0b2c9d1e15ff13e3392933739716feb94d6e430f
Tanat <85154556+Tanat05@users.noreply.github.com>
2024-11-19 10:43:48 +0900

    Update 장바구니분석.r

--- 10주차/장바구니분석.r
+++ 10주차/장바구니분석.r
@@ -9,6 +9,7 @@ library(knitr)
 kable(head(orders, 20))
 
 
+#구매요일 분석
 install.packages("dplyr")
 library(dplyr)
 orders %>%
@@ -16,6 +17,7 @@ orders %>%
   summarise(count=n())
 
 
+#구매간격 시각화
 install.packages("ggplot2")
 library(ggplot2)
 orders %>%
@@ -24,6 +26,7 @@ orders %>%
   ggplot(aes(x=days_since_prior_order)) + geom_histogram(stat="count", fill="red")
 
 
+#구매간격별 구매량 분석
 order_products %>%
   group_by(order_id) %>%
   summarize(n_items = last(add_to_cart_order))
@@ -32,6 +35,7 @@ order_products %>%
   summarize(n_items = last(add_to_cart_order))
 
 
+#구매량 시각화
 order_products %>%
   group_by(order_id) %>%
   summarize(n_items = last(add_to_cart_order)) %>%
@@ -43,12 +47,13 @@ order_products %>%
   summarize(n_items = last(add_to_cart_order))
 
 
+#상위 10개 품목 찾기
 tmp <- order_products %>%
   group_by(product_id) %>%
   summarize(count=n()) %>%
   top_n(10, wt=count)
 tmp
-merge(subset(products, select = c("product_id", "product_name"))) %>%
+merge(subset(products, select=c("product_id", "product_name"))) %>%
   arrange(desc(count))
 order_products %>%
   group_by(product_id) %>%
@@ -57,6 +62,8 @@ order_products %>%
   merge(subset(products, select=c("product_id", "product_name"))) %>%
   arrange(desc(count))
 
+
+#상품 시각화
 tmp <- products %>%
   group_by(department_id, aisle_id) %>%
   summarize(n=n())
@@ -67,7 +74,7 @@ tmp <- tmp %>%
   left_join(departments, by="department_id")
 tmp
 
-aislse <- read.csv("aisles.csv")
+aisles <- read.csv("aisles.csv")
 tmp <- tmp %>%
   left_join(aisles, by="aisle_id")
 tmp
