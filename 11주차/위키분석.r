34ec6881738518ede577d1a46eead6aa94dbc881
Tanat <85154556+Tanat05@users.noreply.github.com>
2024-11-26 11:00:41 +0900

    Create 위키분석.r

---
+++ 11주차/위키분석.r
@@ -0,0 +1,41 @@
+setwd("C:/rtemp")
+train <- read.csv("train_2.csv")
+sum(is.na(train))
+sum(is.na(train))/(ncol(train)*nrow(train))
+
+mediawiki <- train %>%
+  filter(str_detect(Page, "mediawiki"))
+wikimedia <- train %>%
+  filter(str_detect(Page, "wikimedia"))
+wikipedia <- train %>%
+  filter(str_detect(Page, "wikipedia"))
+
+install.packages("tidyverse")
+library(tidyverse)
+df <- data.frame(x=c("a", "a b", "a b c", NA))
+df %>%
+  separate(x, c("a", "b"))
+
+mediawiki %>%
+  separate(Page, into=c("article", "bar"), sep="_www.mediawiki.org_")
+
+mediawiki %>%
+  separate(Page, into=c("article", "bar"), sep="_www.mediawiki.org_") %>%
+  separate(bar, into=c("access", "agent"), sep="_")
+
+mediawiki %>%
+  separate(Page, into=c("article", "bar"), sep="_www.mediawiki.org_") %>%
+  separate(bar, into=c("access", "agent"), sep="_") %>%
+  add_column(locale="medwik")
+
+mediawiki <- mediawiki %>%
+  separate(Page, into=c("article", "bar"), sep="_www.mediawiki.org_") %>%
+  separate(bar, into=c("access", "agent"), sep="_") %>%
+  add_column(locale="medwik")
+mediawiki
+
+wikimedia <- wikimedia %>%
+  separate(Page, into=c("article", "bar"), sep="_www.wikimedia.org_") %>%
+  separate(bar, into=c("access", "agent"), sep="_") %>%
+  add_column(locale="wikimedia")
+wikimedia
