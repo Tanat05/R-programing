66d92dc679df911bbbd6eb785b8e394fbd61c1bc
Tanat <85154556+Tanat05@users.noreply.github.com>
2024-12-02 13:34:01 +0900

    Update 장바구니분석.r

--- 10주차/장바구니분석.r
+++ 10주차/장바구니분석.r
@@ -1,142 +1,84 @@
+
 setwd("C:/rtemp")
-orders <- read.csv("orders.csv")
-products <- read.csv("products.csv")
-order_products <- read.csv("order_products__prior.csv")
+train <- read.csv("train_2.csv")
+sum(is.na(train))
+sum(is.na(train))/(ncol(train)*nrow(train))
+
+mediawiki <- train %>%
+  filter(str_detect(Page, "mediawiki"))
+wikimedia <- train %>%
+  filter(str_detect(Page, "wikimedia"))
+wikipedia <- train %>%
+  filter(str_detect(Page, "wikipedia"))
+
+install.packages("tidyverse")
+library(tidyverse)
+df <- data.frame(x=c("a", "a b", "a b c", NA))
+df %>%
+  separate(x, c("a", "b"))
+
+mediawiki %>%
+  separate(Page, into=c("article", "bar"), sep="_www.mediawiki.org_")
+
+mediawiki %>%
+  separate(Page, into=c("article", "bar"), sep="_www.mediawiki.org_") %>%
+  separate(bar, into=c("access", "agent"), sep="_")
+
+mediawiki %>%
+  separate(Page, into=c("article", "bar"), sep="_www.mediawiki.org_") %>%
+  separate(bar, into=c("access", "agent"), sep="_") %>%
+  add_column(locale="medwik")
+
+mediawiki <- mediawiki %>%
+  separate(Page, into=c("article", "bar"), sep="_www.mediawiki.org_") %>%
+  separate(bar, into=c("access", "agent"), sep="_") %>%
+  add_column(locale="medwik")
+mediawiki
+
+wikimedia <- wikimedia %>%
+  separate(Page, into=c("article", "bar"), sep="_www.wikimedia.org_") %>%
+  separate(bar, into=c("access", "agent"), sep="_") %>%
+  add_column(locale="wikimedia")
+wikimedia
+
+wikipedia <- wikipedia %>%
+  separate(Page, into=c("foo", "bar"), sep=".wikipedia.org_")
+
+wikipedia %>%
+  separate(Page, into=c("foo", "bar"), sep=".wikipedia.org") %>%
+  separate(foo, into=c("article", "locale"), sep=-3) %>%
+  mutate(locale=str(sub(locale,2,3)))
+
+wikipedia %>%
+  separate(Page, into=c("foo", "bar"), sep=".wikipedia.org") %>%
+  separate(foo, into=c("article", "locale"), sep=-3) %>%
+  mutate(locale=str(sub(locale,2,3))) %>%
+  separate(bar, into=c("access", "agent"), sep="_")
+
+tpages <- c(wikipedia, wikimedia, mediawiki)
+tpages %>%
+  filter(grep("north.korea", article))
+
+test <- tpages %>%
+  filter(str_detect(article, "2NE1"))
+tpages[grep("2NE1", tpages$article), ]
+
+test2 <- test %>%
+  melt(id=c("article", "locale", "access", "agent"))
+
+test2 <- test2 %>%
+  mutate(date2=substing(variable, 2)) %>%
+  mutate(data3=as.Date(data2, format="&Y%m%d"))
+
+test2$yr_month <- format(test2$date3, "%Y-%m")
+
+summary <- test2 %>%
+  group_by(access, yr_month) %>%
+  summarize(view=sum(value))
 
 
-install.packages("knitr")
-library(knitr)
-kable(head(orders, 20))
-
-
-#구매요일 분석
-install.packages("dplyr")
-library(dplyr)
-orders %>%
-  group_by(order_dow) %>%
-  summarise(count=n())
-
-
-#구매간격 시각화
-install.packages("ggplot2")
-library(ggplot2)
-orders %>%
-  ggplot(aes(x=order_dow)) + geom_histogram(stat="count", fill="red")
-orders %>%
-  ggplot(aes(x=days_since_prior_order)) + geom_histogram(stat="count", fill="red")
-
-
-#구매간격별 구매량 분석
-order_products %>%
-  group_by(order_id) %>%
-  summarize(n_items = last(add_to_cart_order))
-order_products %>%
-  group_by(order_id) %>%
-  summarize(n_items = last(add_to_cart_order))
-
-
-#구매량 시각화
-order_products %>%
-  group_by(order_id) %>%
-  summarize(n_items = last(add_to_cart_order)) %>%
-  ggplot(aes(x=n_items)) + geom_histogram(stat="count", fill="red")
-
-#그래프를 표로
-order_products %>%
-  group_by(order_id) %>%
-  summarize(n_items = last(add_to_cart_order))
-
-
-#상위 10개 품목 찾기
-tmp <- order_products %>%
-  group_by(product_id) %>%
-  summarize(count=n()) %>%
-  top_n(10, wt=count)
-tmp
-merge(subset(products, select=c("product_id", "product_name"))) %>%
-  arrange(desc(count))
-order_products %>%
-  group_by(product_id) %>%
-  summarize(count=n()) %>%
-  top_n(10, wt=count) %>%
-  merge(subset(products, select=c("product_id", "product_name"))) %>%
-  arrange(desc(count))
-
-
-#상품 시각화
-tmp <- products %>%
-  group_by(department_id, aisle_id) %>%
-  summarize(n=n())
-head(tmp)
-
-departments <- read.csv("departments.csv")
-tmp <- tmp %>%
-  left_join(departments, by="department_id")
-tmp
-
-aisles <- read.csv("aisles.csv")
-tmp <- tmp %>%
-  left_join(aisles, by="aisle_id")
-tmp
-
-
-install.packages("treemap")
-library(treemap)
-
-treemap(tmp, index=c("department", "aisle_id"), vSize="n", vColor="department")
-
-orders %>%
-  group_by(user_id) %>%
-  mutate(max=max(row_number())) %>%
-  subset(max==order_number) %>%
-  subset(select=-max)
-
-
-  count(order_number) %>%
-  ungroup() %>%
-  count(order_number)
-
-a <- orders %>%
-  group_by(user_id) %>%
-  mutate(max=max(row_number())) %>%
-  subset(max==order_number) %>%
-  subset(select=-max) %>%
-  ungroup() %>%
-  count(order_number)
-a
-
-#잘못됨
-orders %>%
-  group_by(user_id) %>%
-  subset(order_number==max(order_number)) %>%
-  ungroup() %>%
-  count(order_number)
-
-a %>% ggplot(aes(order_number, n)) + geom_line(color="red", size=1) + geom_point(size=2, color="red")
-
-group_by(product_id)
-summarize(reordered_count=sum(reordered))
-
-summarize(reordered_count=sum(reordered), count=n())
-mutate(proportion=reordered_count/100)
-
-top_n(100, wt=count) %>%
-  top_n(10, wt=proportion)
-
-merge(subset(products, select=c("product_id", "product_name")))
-arrange(desc(proportion))
-
-tmp <- order_products %>%
-  dplyr::group_by(product_id) %>%
-  summarize(reordered_count=sum(reordered), count=n()) %>%
-  mutate(proportion=reordered_count/count*100) %>%
-  top_n(10, wt=proportion)
-  
-tmp <- order_products %>%
-  dplyr::group_by(product_id) %>%
-  summarize(reordered_count=sum(reordered), count=n()) %>%
-  mutate(proportion=reordered_count/count*100) %>%
-  top_n(100, wt=proportion) %>%
-  top_n(20, wt=proportion) %>%
-  merge(subset(products, select=c("product_id", "product_name"))) %>%
-  arrange(desc(proportion))
+summary %>%
+  ggplot(aes(yr_month, view, color=access))
+geom_line(aes(group=access))
+geom_point()
+theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5))
