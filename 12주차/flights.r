cac73da28cd1509363f89dd4f1be477a73df68ef
Tanat <85154556+Tanat05@users.noreply.github.com>
2024-12-03 11:15:45 +0900

    Update flights.r

--- 12주차/flights.r
+++ 12주차/flights.r
@@ -4,3 +4,20 @@ install.packages("tidyverse")
 library(tidyverse)
 
 flights <- read.csv("flights.csv")
+
+flights %>%
+  .$AIRLINE
+flights %>%
+  .[["AIRLINE"]]
+
+by_test <- group_by(flights, DESTINATION_AIRPORT)
+delay <- summarise(by_test, count = n(), dist = mean(DISTANCE, na.rm = TRUE), delay = mean(ARRIVAL_DELAY, na.rm = TRUE))
+delay
+
+select(flights, contains("TIME"))
+
+flights_sml <- select(flights, YEAR:DAY, ends_with("DELAY"), DISTANCE)
+flights_sml
+
+flights %>%
+  filter(between(ARRIVAL_DELAY, 0, 60))
