00f3d6c9876a5aafd06c5cc294142447990990f5
Tanat <85154556+Tanat05@users.noreply.github.com>
2024-12-03 10:54:02 +0900

    Update 위키분석.r

--- 11주차/위키분석.r
+++ 11주차/위키분석.r
@@ -1,84 +1,70 @@
-setwd("C:/rtemp")
+setwd("c:/r_temp")
 train <- read.csv("train_2.csv")
+library(dplyr)
+library(stringr)
+
+nrow(train)
+ncol(train)
+glimpse(train)
+
 sum(is.na(train))
 sum(is.na(train))/(ncol(train)*nrow(train))
 
-mediawiki <- train %>%
-  filter(str_detect(Page, "mediawiki"))
-wikimedia <- train %>%
-  filter(str_detect(Page, "wikimedia"))
-wikipedia <- train %>%
-  filter(str_detect(Page, "wikipedia"))
+sum(sapply(train, is.numeric))
 
+mediawiki<-train%>%filter(str_detect(Page,"mediawiki"))
+wikimedia<-train%>%filter(str_detect(Page,"wikimedia"))
+wikipedia<-train%>%filter(str_detect(Page,"wikipedia"))
+mediawiki
 install.packages("tidyverse")
 library(tidyverse)
-df <- data.frame(x=c("a", "a b", "a b c", NA))
-df %>%
-  separate(x, c("a", "b"))
-
-mediawiki %>%
-  separate(Page, into=c("article", "bar"), sep="_www.mediawiki.org_")
-
-mediawiki %>%
-  separate(Page, into=c("article", "bar"), sep="_www.mediawiki.org_") %>%
-  separate(bar, into=c("access", "agent"), sep="_")
-
-mediawiki %>%
-  separate(Page, into=c("article", "bar"), sep="_www.mediawiki.org_") %>%
-  separate(bar, into=c("access", "agent"), sep="_") %>%
-  add_column(locale="medwik")
-
-mediawiki <- mediawiki %>%
-  separate(Page, into=c("article", "bar"), sep="_www.mediawiki.org_") %>%
-  separate(bar, into=c("access", "agent"), sep="_") %>%
-  add_column(locale="medwik")
+mediawiki<-mediawiki%>%
+  separate(Page,into =c ("article","bar"),sep="_www.mediawiki.org_")%>%
+  separate(bar,into=c("access","agent"),sep="_")%>%
+  add_column(locale="medawik")
 mediawiki
-
-wikimedia <- wikimedia %>%
-  separate(Page, into=c("article", "bar"), sep="_www.wikimedia.org_") %>%
-  separate(bar, into=c("access", "agent"), sep="_") %>%
+#연습문제 
+#1
+wikimedia<-wikimedia%>%
+  separate(Page,into =c ("article","bar"),sep="_www.wikimedia.org_")%>%
+  separate(bar,into=c("access","agent"),sep="_")%>%
   add_column(locale="wikimedia")
-wikimedia
+#Page필드에서 정보 추출
+wikipedia<-wikipedia%>%
+  separate(Page,into=c("foo","bar"),sep=".wikipedia.org_")%>%
+  separate(foo,into=c("article","locale"),sep=-3)%>%
+  mutate(locale=str_sub(locale,2,3))%>%
+  separate(bar,into=c("access","agent"),sep="_")
+save.image("wiki.Rdata")
+load("wiki.Rdata")
+save.image("wiki.Rdata")
+setwd("C:/r_temp")
+tpages <- rbind(wikipedia,wikimedia,mediawiki)
 
-wikipedia <- wikipedia %>%
-  separate(Page, into=c("foo", "bar"), sep=".wikipedia.org_")
+test <- tpages %>% filter(row_number()%in%grep("north.korea",article))
 
-wikipedia %>%
-  separate(Page, into=c("foo", "bar"), sep=".wikipedia.org") %>%
-  separate(foo, into=c("article", "locale"), sep=-3) %>%
-  mutate(locale=str(sub(locale,2,3)))
+tpages %>% filter(str_detect(article,"North.Korea"))
+#월별로 방문 건수를 그래프로 표현
+test<-tpages %>% filter(str_detect(article,"2NE1"))
+tpages[grep("2NE1",tpages$article), ]
+library(reshape2)
+test2<-test%>%melt(id=c("article","locale","access","agent"))
+#x를 제거  
+test2<-test2%>%mutate(data2=substring(variable,2)) %>%
+  mutate(data3=as.Date(data2,format="%Y.%m.%d"))
+test2$yr_month<-format(test2$data3,"Y-%m")
 
-wikipedia %>%
-  separate(Page, into=c("foo", "bar"), sep=".wikipedia.org") %>%
-  separate(foo, into=c("article", "locale"), sep=-3) %>%
-  mutate(locale=str(sub(locale,2,3))) %>%
-  separate(bar, into=c("access", "agent"), sep="_")
-
-tpages <- cbind(wikipedia, wikimedia, mediawiki)
-tpages %>%
-  filter(grep("north.korea", article))
-
-test <- tpages %>%
-  filter(str_detect(article, "2NE1"))
-tpages[grep("2NE1", tpages$article), ]
-
-test2 <- test %>%
-  melt(id=c("article", "locale", "access", "agent"))
-
-test2 <- test2 %>%
-  mutate(date2=substing(variable, 2)) %>%
-  mutate(data3=as.Date(data2, format="&Y%m%d"))
+test2 <- test2 %>% mutate(date2 = substring(variable,2)) %>%
+  mutate(date3 = as.Date(date2, format = "%Y.%m.%d"))
 
 test2$yr_month <- format(test2$date3, "%Y-%m")
-
 summary <- test2 %>%
-  group_by(access, yr_month) %>%
-  summarize(view=sum(value))
+  group_by(access,yr_month) %>%
+  summarize(view = sum(value))
 
+library(ggplot2)
 
-summary %>%
-  ggplot(aes(yr_month, view, color=access))
-geom_line(aes(group=access))
-geom_point()
-theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5))
-
+summary%>%
+  ggplot(aes(yr_month,view,color=access)) +
+  geom_line(aes(group = access)) +
+  geom_point()
